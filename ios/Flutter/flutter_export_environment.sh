4fa5e067ec10ab9e750332e5bbff389d9655a9d6
marisabelc <marisabelchang@gmail.com>
2023-01-31 16:49:14 -0800

    set up Firebase, login, signup, course pages

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\kiana\AndroidStudiosProjects\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\kiana\AndroidStudioProjects\JesonProject"
+export "FLUTTER_ROOT=C:\Users\maris\Desktop\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\maris\OneDrive\Escritorio\jeson_flutter_app"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
